3653e58069d303c1238a329b5dcadd8e49453c12
kirillbelykh <kirillbelykh228@gmail.com>
2025-08-22 21:27:02 +0300

    Change cells.py model

--- app/models/cells.py
+++ app/models/cells.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 from database.db import Base
 
