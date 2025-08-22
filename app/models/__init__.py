3eb472f6c5c47c8e9f533c2b18aeac89f68bb0bd
kirillbelykh <kirillbelykh228@gmail.com>
2025-08-22 21:30:50 +0300

    Made batches.py model

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.items import Item
+from app.models.cells import Cell   
+from app.models.batches import Batch
+
+__all__ = ['Batch', 'Cell', 'Item']
--- app/models/batches.py
+++ app/models/batches.py
@@ -1,1 +1,17 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database.db import Base
+
+class Batch(Base):
+    __tablename__ = 'batches'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+    quantity = Column(Float, nullable=False)
+
+    cell_id = Column(Integer, ForeignKey('cells.id'), nullable=False)
+    cells = relationship("Cell", back_populates="batch")
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    items = relationship("Item", back_populates="batch")
+    
