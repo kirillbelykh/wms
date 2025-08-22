6ed2db91ebbb4eebc56923a06a8bc9cbb2ff7b0d
kirillbelykh <kirillbelykh228@gmail.com>
2025-08-22 21:24:49 +0300

    Made items.py model

---
+++ app/models/items.py
@@ -0,0 +1,17 @@
+from database.db import Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+    quantity = Column(Float, default=0.0)
+    
+    #relationships
+    cells = relationship("Cell", back_populates="item")
+    batches = relationship("Batch", back_populates="item")
+    
+    
