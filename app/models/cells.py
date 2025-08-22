38b62fe6ff3dd75eb8967f82907835c8a075afaa
kirillbelykh <kirillbelykh228@gmail.com>
2025-08-22 21:26:12 +0300

    Made cells.py model

---
+++ app/models/cells.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
+from database.db import Base
+
+class Cell(Base):
+    __tablename__ = 'cells'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+    capacity = Column(Float, default=0.0)
+
+    # relationships
+    item_id = Column(Integer, ForeignKey('items.id'))
+    item = relationship("Item", back_populates="cells")
+    batches = relationship("Batch", back_populates="cell")
+    
+    
