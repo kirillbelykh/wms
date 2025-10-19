07f9e9171e8fc5815e2f282f732e6a341980ca2f
kirillbelykh <kirillbelykh228@gmail.com>
2025-10-19 18:54:48 +0300

    working on inventory

--- app/api/catalogs/inventory.py
+++ app/api/catalogs/inventory.py
@@ -1,19 +1,22 @@
 from fastapi import APIRouter, Request, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy.orm import joinedload
 from fastapi.responses import HTMLResponse
 from app.database import get_db
 from app.models import Item, Cell, Batch
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter(prefix="/catalogs/inventory", tags=["Inventory"])
-templates = Jinja2Templates(directory="templates")
+router = APIRouter(prefix="/catalogs/inventory", tags=["catalogs:inventory"])
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def inventory_list(request: Request, db: Session = Depends(get_db)):
     items = (
         db.query(Item)
-        .outerjoin(Item.cell)
-        .outerjoin(Item.batch)
+        .options(
+            joinedload(Item.cell),
+            joinedload(Item.batch)
+        )
         .all()
     )
 
